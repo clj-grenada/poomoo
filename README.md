cd7c21bae2f34e18a8c964e8b7f510c60feafa7e
Richard Möhn <richard.moehn@posteo.de>
2015-08-09 16:41:39 +0900

    Add some boilerplate

---
+++ README.md
@@ -0,0 +1,9 @@
+# poomoo
+
+Bars and tools for working with Clojure Grenada Things.
+
+See also https://github.com/clj-grenada/grenada-spec.
+
+## License
+
+See [LICENSE.txt](LICENSE.txt).
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject poomoo "0.1.0-SNAPSHOT"
+  :description "A collection of Bars and tools for Clojure Grenada Things"
+  :url "https://github.com/clj-grenada/poomoo"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
